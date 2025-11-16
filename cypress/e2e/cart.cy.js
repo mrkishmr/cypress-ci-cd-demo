5638184acade3dc47a9efa8d85df2ca727d4bd79
mrkishmr <mrkishan1209@gmail.com>
2025-11-16 18:31:11 +0530

    Cypress project init

---
+++ cypress/e2e/cart.cy.js
@@ -0,0 +1,10 @@
+describe('Cart test', () => {
+  it('should add item to cart', () => {
+    cy.visit('https://www.saucedemo.com/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('.inventory_item').first().find('button').click();
+    cy.get('.shopping_cart_badge').should('contain', '1');
+  });
+});
